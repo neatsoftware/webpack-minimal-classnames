c2e008700d07ce6df69233e8cf44025c7415b54c
gpoitch <garth22@gmail.com>
2019-06-05 09:00:35 -0400

    Upgrade deps

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const output = require('../dist/test-output')
 
 describe(pkg.name, () => {
   it('genereates unique minimal classnames, unique transformed names if original classnames the same in different file, and references the same classname when imported multiple times', () => {
-    assert.deepEqual(output, [
+    assert.deepStrictEqual(output, [
       'a', 'b', // style1.css
       'c', // style2.css (contains same classname, but in different file/module
       'a', 'b' // re-imported style1.css again and classes should be the same
